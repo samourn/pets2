d9e467ef0979c731fa22db48535449a918433d7b
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-01-18 14:13:53 -0500

    added pet image routes

--- index.php
+++ index.php
@@ -5,10 +5,30 @@
     // create instance of base class
     $f3 = Base::instance();
 
-    // define a route using a template
+    // set debug level
+    $f3->set('DEBUG', 3);
+
+    // define a default route
     $f3->route('GET /', function() {
-        $template = new Template();
-        echo $template->render('views/home.html');
+            $template = new Template();
+            echo $template->render('views/home.html');
+        }
+    );
+
+    // define a route using parameters
+    $f3->route('GET /pets/show/@pet', function($f3, $params) {
+            switch($params['pet']) {
+                case 'dog':
+                    echo '<img src="https://s7d1.scene7.com/is/image/PETCO/dog-category-090617-369w-269h-hero-cutout-d?fmt=png-alpha" alt="dog">'; break;
+                case 'cat':
+                    echo '<img src="http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg" alt="cat">'; break;
+                case 'hippo':
+                    echo '<img src="http://ichef.bbci.co.uk/wwfeatures/wm/live/624_351/images/live/p0/3d/tk/p03dtkw2.jpg" alt="hippo">'; break;
+                case 'phoenix':
+                    echo '<img src="https://theswisspub.com/wp-content/uploads/2017/07/THE-PHOENIX.jpg" alt="phoenix">'; break;
+                default:
+                    $f3->error(404);
+            }
         }
     );
 
