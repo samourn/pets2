079c50e6ab68e8107b03989da2aa6c8945ed6c8b
Sam Ourn <sourn@mail.greenriver.edu>
2018-01-25 14:41:29 -0500

    Added validString()

--- model/validate.php
+++ model/validate.php
@@ -7,3 +7,9 @@ function validColor($color){
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
+
+function validString($string) {
+    if(!empty($string) && ctype_alpha($string)) {
+        return true;
+    }
+}
